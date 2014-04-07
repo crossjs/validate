d65abdcf1bb92ad7f4c9d3b794334b5c0c4d7217
crossjs <one@ffully.com>
2014-04-08 00:18:14 +0800

    1.0.0 release

--- src/validate.js
+++ src/validate.js
@@ -8,27 +8,20 @@ define(function (require, exports, module) {
 'use strict';
 
 var $ = require('$'),
-  Class = require('class'),
-
-  Config = require('./config');
+  Widget = require('widget');
 
 var htmldecode = function (value) {
   var replacements = {
-    '&lt;': '<',
-    '&gt;': '>',
-    '&amp;': '&',
-    '&quot;': '"',
-    '&apos;': '\''
+    'lt': '<',
+    'gt': '>',
+    'amp': '&',
+    'quot': '"',
+    'apos': '\''
   };
-  return value.replace(/&(?:lt|gt|amp|quot|apos);/g, function(character) {
-    return replacements[character];
+  return value.replace(/&(?:lt|gt|amp|quot|apos);/g, function(character, $1) {
+    return replacements[$1];
   });
 };
-
-var validateElems = Config.elements.join(','),
-  validateAttrs = Config.attributes,
-  validateRules = Config.rules,
-  errorMessages = Config.messages;
 
 /**
  * Validate
@@ -38,105 +31,158 @@ var validateElems = Config.elements.join(','),
  */
 var Validate = Widget.extend({
 
-  setup: function () {
-    var self = this,
-      form = $(options.form);
-
-    if (form.length === 0) {
-      return;
-    }
-
-    self.rules = options.rules || {};
-    self.messages = options.messages || {};
-
-    self.pending = 0;
-    self.submitted = false;
-
-    self.errorElements = $();
-
-    self.form = form
-      .attr({
-        novalidate: 'novalidate'
-      })
-      .on('submit', function (e) {
-        // e.preventDefault();
-        if (self.submitted) {
-          // 判断是否等待异步
-          self.checkPending();
-        } else {
-          self.validateForm();
+  defaults: {
+    // 需要验证的元素
+    elements: ['select', 'option', 'input[type="checkbox"]', 'input[type="radio"]', 'textarea', 'input[type="text"]', 'input[type="password"]', 'input[type="file"]'],
+    // 默认查找的属性
+    attributes: ['required', 'pattern', 'minlength', 'maxlength', 'min', 'max', 'equalto', 'minto', 'maxto', 'digits', 'number', 'url', 'async'],
+    // 预置的校验函数
+    // params: {elem, name, value, rule, prop}
+    rules: {
+      required: function (params) {
+        var elem = params.elem;
+        // 如果是单复选框，则特殊判断
+        if (/radio|checkbox/.test(elem.prop('type'))) {
+          if (params.name) {
+            return !!this.$('[name="' + params.name + '"]:checked').length;
+          }
+          return elem.is('input[type="checkbox"]');
         }
-        return false;
-      });
+        return (/\S/).test(params.value);
+      },
+      pattern: function (params) {
+        return new RegExp(params.prop).test(params.value);
+      },
+      minlength: function (params) {
+        var len = params.value.length;
+        return len ? (len >= params.prop) : true;
+      },
+      maxlength: function (params) {
+        var len = params.value.length;
+        return len ? (len <= params.prop) : true;
+      },
+      min: function (params) {
+        return parseInt(params.value, 10) >= params.prop;
+      },
+      max: function (params) {
+        return parseInt(params.value, 10) <= params.prop;
+      },
+      equalto: function (params) {
+        return params.value === this.$('[name="' + params.prop + '"]').val();
+      },
+      minto: function (params) {
+        return parseInt(params.value, 10) >= parseInt(this.$('[name="' + params.prop + '"]').val(), 10);
+      },
+      maxto: function (params) {
+        return parseInt(params.value, 10) <= parseInt(this.$('[name="' + params.prop + '"]').val(), 10);
+      },
+      digits: function (params) {
+        return !/\D/.test(params.value);
+      },
+      number: function (params) {
+        return !isNaN(params.value);
+      },
+      url: function (params) {
+        // 此项目暂不支持https
+        return (/^https?:\/\/((?:[\w\d][\w\d\-]*)(?:\.[\w\d][\w\d\-]*)*(?:\.\w{2,})+)(?:\/\S*)?$/i).test(params.value);
+      },
+      // 异步验证，如：从服务端验证、校验动态加载的图片尺寸
+      async: function (params) {
+        this.pendingCount++;
 
-    // 事件订阅
-    if ($.isPlainObject(options.on)) {
-      self.on(options.on);
-    }
+        params.prop.call(this, function (success) {
+          if (success) {
+            this.pendingCount--;
+          } else {
+            this.addError(params);
+          }
+          // 检查
+          this.checkValidation();
+        });
 
-    if (options.eventType) {
-      self.initElements(options.eventType);
+        return 'pending';
+      }
+    },
+    messages: {
+      'required': '此项必填',
+      // TODO: 从title中获取
+      'pattern': '不符合规则',
+      'minlength': '请输入至少{0}个字符',
+      'maxlength': '最多输入{0}个字符',
+      'min': '不能小于{0}',
+      'max': '不能大于{0}',
+      'equalto': '两次输入的内容不一致，请重新输入',
+      'minto': '不能小于{0}',
+      'maxto': '不能大于{0}',
+      // 'password': '密码只能包含ASCII字符',
+      'digits': '请输入数字',
+      'number': '请输入数字',
+      'url': '请输入URL地址',
+      'async': '异步校验失败，请检查'
+    },
+    customRules: {
+    },
+    delegates: function () {
+      var delegates = {
+          'submit': function (e) {
+            e.preventDefault();
+            if (this.data('submitted')) {
+              // 判断是否等待异步
+              this.checkValidation();
+            } else {
+              this.validateForm();
+            }
+            // return false;
+          }
+        },
+        // 元素事件
+        key = this.option('eventType');
+
+      if (key) {
+        key += ' ' + this.option('elements').join(',');
+
+        delegates[key] = function (e) {
+          console.log(arguments);
+          this.validateElem($(e.currentTarget));
+        };
+      }
+
+      return delegates;
+    },
+    events: {
+      valid: function () {
+        this.element.submit();
+      }
     }
   },
 
-  /**
-   * 初始化表单元素事件绑定
-   * @method initElements
-   * @param {String} eventType 事件类型
-   */
-  initElements: function (eventType) {
-    var self = this;
-    self.form.find(validateElems)
-      .off('.validate')
-      .on(eventType + '.validate', function () {
-        self.validateElem($(this));
+  setup: function () {
+    this.state(Validate.STATE.INITIAL);
+
+    this.pendingCount = 0;
+    this.errorElements = $();
+
+    this.element.attr({
+        novalidate: 'novalidate'
       });
   },
 
   /**
    * 校验整个表单
+   *
    * @method validateForm
    */
   validateForm: function () {
     var self = this;
-    self.errorElements = $();
-    self.submitted = true;
-    self.form.find(validateElems)
+    this.errorElements = $();
+    this.data('submitted', true);
+    this.$(this.option('elements').join(','))
       .filter(':enabled')
       .each(function () {
         self.validateElem($(this));
       });
-    self.checkPending();
-    // self.form.submit();
-  },
-
-  /**
-   * 检查异步完成情况并提交表单
-   * @method checkPending
-   */
-  checkPending: function () {
-    if (this.pending === 0) {
-      this.submitted = false;
-      if (this.isValid()) {
-        this.fire('valid');
-      } else {
-        this.focusError();
-        this.fire('error');
-      }
-    }
-  },
-
-  /**
-   * 清除错误信息
-   * @method clearErrors
-   */
-  clearErrors: function () {
-    var self = this;
-    self.form.find(validateElems)
-      .filter(':enabled')
-      .each(function () {
-        self.removeError($(this));
-      });
+    this.checkValidation();
+    // this.element.submit();
   },
 
   /**
@@ -145,41 +191,40 @@ var Validate = Widget.extend({
    * @method validateElem
    */
   validateElem: function (elem) {
-    var self = this,
-      valid = true,
+    var valid = true, rules,
 
-      name = elem.prop('name'),
-      value = elem.val();
+      params = {
+        elem: elem,
+        name: elem.prop('name'),
+        value: elem.val()
+      };
 
     // 验证前，先移除错误信息
-    self.removeError(elem);
+    this.removeError(params);
 
     // 验证属性
-    $.each(validateAttrs, function (index, rule) {
+    $.each(this.option('attributes'), $.proxy(function (index, rule) {
       var prop = elem.attr(rule);
       if (prop !== undefined && prop !== null && prop !== '') {
-        prop = isNaN(prop) ? prop : +prop;
-        valid = self.checkRule(name, rule, elem, value, prop);
-        if (!valid) {
-          return false;
-        }
+        return (valid = this.checkRule($.extend({
+            rule: rule,
+            prop: isNaN(prop) ? prop : +prop
+          },params)));
       }
-    });
+    }, this));
 
     if (valid === false) {
       return;
     }
 
-    // JS里定义的rules
-    if (self.rules[name]) {
-      $.each(self.rules[name], function (rule, prop) {
-        DEBUG && console.log(rule);
-        prop = isNaN(prop) ? prop : +prop;
-        valid = self.checkRule(name, rule, elem, value, prop);
-        if (!valid) {
-          return false;
-        }
-      });
+    // 自定义的rules
+    if ((rules = this.option(['customRules', params.name].join('/')))) {
+      $.each(rules, $.proxy(function (rule, prop) {
+        return this.checkRule($.extend({
+            rule: rule,
+            prop: isNaN(prop) ? prop : +prop
+          },params));
+      }, this));
 
       // if (valid === false) {
       //  return;
@@ -190,75 +235,88 @@ var Validate = Widget.extend({
 
   /**
    * 校验是否符合指定规则
-   * @param {Object} name 表单元素
-   * @param {String} rule 规则名
-   * @param {Object} elem 表单元素
-   * @param {String} value 表单元素值
-   * @param {function|string} prop 验证方法或基准值
-   * @return {boolean}
+   *
    * @method checkRule
+   *
+   * @param {Object} name 表单元素，rule 规则名，elem 表单元素，value 表单元素值，prop 验证方法或基准值
+   * @return {boolean}
    */
-  checkRule: function (name, rule, elem, value, prop) {
-    var valid;
-    if (typeof prop === 'function') {
-      valid = prop.call(this, elem, value);
-    } else if (validateRules[rule]) {
-      valid = validateRules[rule].call(this, elem, value, prop);
+  checkRule: function (params) {
+    var valid, rule = this.option(['rules', params.rule].join('/'));
+
+    // params.elem, params.value, params.prop
+
+    if (rule) {
+      valid = rule.call(this, params);
+    } else if (typeof params.prop === 'function') {
+      valid = params.prop.call(this, params);
     }
-    if (!valid) {
-      this.addError(elem, this.getMessage(name, rule, prop));
+
+    if (valid === false) {
+      this.addError(params);
     }
+
     return valid;
   },
 
   /**
-   * 获取错误提示信息
-   * @param {String} name 表单元素的name值
-   * @param {String} rule 当前错误对应的娇艳规则
-   * @param {String} prop 校验的基准值，比如maxlength="3"中的”3“
-   * @method getMessage
+   * 检查校验完成情况
+   *
+   * @method checkValidation
    */
-  getMessage: function (name, rule, prop) {
-    var text;
-    if (this.messages[name]) {
-      text = this.messages[name][rule];
+  checkValidation: function () {
+    if (this.errorElements.length > 0) {
+      this.state(Validate.STATE.ERROR);
+      this.data('submitted', false);
+      this.focusError();
+      return this.fire('error');
     }
-    if (!text) {
-      text = errorMessages[rule];
+
+    if (this.pendingCount <= 0) {
+      this.state(Validate.STATE.VALID);
+      this.data('submitted', false);
+      return this.fire('valid');
     }
-    return text ? this.formatMessage(text, prop) : '';
+
+    this.state(Validate.STATE.PENDING);
   },
 
   /**
-   * 格式化错误提示信息
-   * TODO: 支持prop值为数组的情况
-   * @param {String} text 错误提示信息
-   * @param {String} prop 校验的基准值
-   * @method formatMessage
+   * 清除错误信息
+   * @method clearErrors
    */
-  formatMessage: function (text, prop) {
-    return text.replace(/\{0\}/g, prop);
+  clearErrors: function () {
+    var self = this;
+    this.$(this.option('elements').join(','))
+      .filter(':enabled')
+      .each(function () {
+        self.removeError({
+          elem: $(this)
+        });
+      });
   },
 
   /**
    * 使第一个校验失败的元素获得焦点
+   *
    * @method focusError
    */
   focusError: function () {
-    if (this.errorElements.length) {
-      this.errorElements.eq(0).focus();
-    }
+    this.errorElements.length && this.errorElements.eq(0).focus();
   },
 
   /**
    * 显示错误信息
    * 如果需要自定义错误显示方式，则在实例里修改此方法。（不要修改原型）
-   * @param {Object} elem 表单元素
-   * @param {String} text 错误信息
+   *
    * @method addError
+   *
+   * @param {Object} params 元素参数
+   * @param {String} [text] 错误信息
    */
-  addError: function (elem, text) {
-    var wrap = elem.data('wrap'),
+  addError: function (params, text) {
+    var elem = params.elem,
+      wrap = elem.data('wrap'),
       help = elem.data('help');
 
     if (!wrap) {
@@ -277,18 +335,36 @@ var Validate = Widget.extend({
     }
 
     wrap.addClass('has-error');
-    help.text(text);
+    help.text(text || this.getMessage(params));
 
     this.errorElements = this.errorElements.add(elem);
   },
 
   /**
-   * 移除错误信息
-   * @param {Object} elem 表单元素
-   * @method removeError
+   * 获取错误提示信息
+   * @param {String} name 表单元素的name值
+   * @param {String} rule 当前错误对应的校验规则
+   * @param {String} prop 校验的基准值，比如maxlength="3"中的”3“
+   * @method getMessage
    */
-  removeError: function (elem) {
-    var wrap = elem.data('wrap'),
+  getMessage: function (params) {
+    var text = this.option(['customMessages', params.name, params.rule].join('/')) ||
+      this.option(['messages', params.name, params.rule].join('/')) ||
+      this.option(['messages', params.rule].join('/'));
+
+    return text ? text.replace(/\{0\}/g, params.prop) : '';
+  },
+
+  /**
+   * 移除错误信息
+   *
+   * @method removeError
+   *
+   * @param {Object} params 表单元素
+   */
+  removeError: function (params) {
+    var elem = params.elem,
+      wrap = elem.data('wrap'),
       help = elem.data('help'),
       placeholder;
 
@@ -317,23 +393,20 @@ var Validate = Widget.extend({
     }
 
     this.errorElements = this.errorElements.not(elem);
-  },
-
-  /**
-   * 表单是否校验通过（或从未校验）
-   * @param {Object} elem 表单元素
-   * @return {boolean}
-   * @method isValid
-   */
-  isValid: function () {
-    return this.pending === 0 && this.errorElements.length === 0;
   }
 });
 
 Validate.addRule = function (rule) {
-  $.extend(true, validateRules, rule);
+  $.extend(true, Validate.prototype.defaults.rules, rule);
 };
 
-return Validate;
+Validate.STATE = {
+  INITIAL: -1,
+  ERROR: 0,
+  VALID: 1,
+  PENDING: 2
+};
+
+module.exports = Validate;
 
 });
